0bb223497f1c3d017c3db583696ad3eb1d4f8cbf
vf <asfanh@gmail.com>
2014-03-07 09:08:56 -0500

    Finish layout and routes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,9 +40,14 @@ Spork.prefork do
     # Run specs in random order to surface order dependencies. If you find an
     # order dependency and want to debug it, you can fix the order by providing
     # the seed, which is printed after each run.
-    #     --seed 1234
+    # --seed 1234
     config.order = "random"
+    # Include the Capybara DSL so that specs in spec/requests still work.
     config.include Capybara::DSL
+    # Disable the old-style object.should syntax.
+    config.expect_with :rspec do |c|
+      c.syntax = :expect
+    end
   end
 end
 
